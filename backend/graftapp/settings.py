6e5647e97eda5a342d6dbe7245ebc7529850fd65
Jacob <tkaczykj@umich.edu>
2025-11-13 15:43:21 -0500

    update settings.py

--- backend/graftapp/settings.py
+++ backend/graftapp/settings.py
@@ -62,19 +62,21 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://graft-6311.onrender.com"
+    "https://graft-6311.onrender.com",
+    "https://graft-chi.vercel.app"
 ]
 
 ROOT_URLCONF = 'graftapp.urls'
